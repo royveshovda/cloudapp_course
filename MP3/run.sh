14904acaad3638555ae2afb4989c51ca4877a844
Roy Veshovda <roy@veshovda.no>
2015-09-08 13:59:48 +0200

    Bugfix

--- MP3/run.sh
+++ MP3/run.sh
@@ -13,4 +13,4 @@ echo "*****     TASK C     *****"
 storm jar target/storm-example-0.0.1-SNAPSHOT.jar TopWordFinderTopologyPartC data.txt > output-part-c.txt
 
 echo "*****     TASK D     *****"
-storm jar target/storm-example-0.0.1-SNAPSHOT-jar TopWordFinderTopologyPartD data.txt > output-part-d.txt
+storm jar target/storm-example-0.0.1-SNAPSHOT.jar TopWordFinderTopologyPartD data.txt > output-part-d.txt
