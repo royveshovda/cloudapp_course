b4716ced12a90fae30b3f4706f8c48de169e53a1
Roy Veshovda <roy@veshovda.no>
2015-09-23 06:20:39 +0200

    Task B

--- MP5/src/KMeansMP.java
+++ MP5/src/KMeansMP.java
@@ -16,29 +16,76 @@ import scala.Tuple2;
 
 
 public final class KMeansMP {
-	// TODO
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println(
-                    "Usage: KMeansMP <input_file> <results>");
-            System.exit(1);
-        }
-        String inputFile = args[0];
-        String results_path = args[1];
-        JavaPairRDD<Integer, Iterable<String>> results;
-        int k = 4;
-        int iterations = 100;
-        int runs = 1;
-        long seed = 0;
-		final KMeansModel model;
-		
-        SparkConf sparkConf = new SparkConf().setAppName("KMeans MP");
-        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+  private static final Pattern SPACE = Pattern.compile(",");
+  public Vector call(String line) {
+    String[] tok = SPACE.split(line);
+    double[] point = new double[tok.length-1]; for (int i = 1; i < tok.length; ++i) {
+    point[i-1] = Double.parseDouble(tok[i]); }
+    return Vectors.dense(point); }
+  }
 
-        //TODO
-
-        results.saveAsTextFile(results_path);
-
-        sc.stop();
+  private static class ParseTitle implements Function<String, String> {
+    private static final Pattern SPACE = Pattern.compile(",");
+    public String call(String line) {
+      String[] tok = SPACE.split(line); return tok[0];
     }
+  }
+
+  private static class PrintCluster implements VoidFunction<Tuple2<Integer, Iterable<String>>> {
+    private KMeansModel model;
+    public PrintCluster(KMeansModel model) {
+      this.model = model;
+    }
+
+    public void call(Tuple2<Integer, Iterable<String>> Cars) throws Exception {
+      String ret = "[";
+      for(String car: Cars._2()){
+        ret += car + ", ";
+      }
+      System.out.println(ret + "]");
+    }
+  }
+
+  private static class ClusterCars implements PairFunction<Tuple2<String, Vector>, Integer, String> {
+    private KMeansModel model;
+    public ClusterCars(KMeansModel model) {
+      this.model = model;
+    }
+
+    public Tuple2<Integer, String> call(Tuple2<String, Vector> args) {
+      String title = args._1();
+      Vector point = args._2();
+      int cluster = model.predict(point);
+      return new Tuple2<Integer, String>(cluster, title);
+    }
+  }
+  
+  // TODO
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("Usage: KMeansMP <input_file> <results>");
+      System.exit(1);
+    }
+    String inputFile = args[0];
+    String results_path = args[1];
+    JavaPairRDD<Integer, Iterable<String>> results;
+    int k = 4;
+    int iterations = 100;
+    int runs = 1;
+    long seed = 0;
+    final KMeansModel model;
+	
+    SparkConf sparkConf = new SparkConf().setAppName("KMeans MP");
+    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+    //TODO
+
+    JavaRDD<Vector> points = lines.map(new ParsePoint()); JavaRDD<String> titles = lines.map(new ParseTitle());
+    KMeansModel model = KMeans.train(points.rdd(), k, iterations, runs, KMeans.RANDOM(), 0);
+    JavaPairRDD<Integer, Iterable<String>> results =titles.zip(points).mapToPair(new ClusterCars(model)).groupByKey();
+
+    results.saveAsTextFile(results_path);
+
+    sc.stop();
+  }
 }
