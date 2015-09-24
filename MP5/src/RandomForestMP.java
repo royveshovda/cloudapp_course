337f5010af9fee71e391e03e0ca862a2e0040e03
Roy Veshovda <roy@veshovda.no>
2015-09-24 05:12:58 +0200

    Task D

--- MP5/src/RandomForestMP.java
+++ MP5/src/RandomForestMP.java
@@ -21,9 +21,9 @@ public final class RandomForestMP {
         public LabeledPoint call(String line) throws Exception {
             String[] tok = SPACE.split(line);
             int label = Integer.parseInt(tok[tok.length-1]);
-            int[] point = new int[tok.length-1];
+            double[] point = new double[tok.length-1];
             for (int i = 0; i < tok.length - 1; ++i) {
-                point[i] = Integer.parseInt(tok[i]);
+                point[i] = Double.parseDouble(tok[i]);
             }
             return new LabeledPoint(label, Vectors.dense(point));
         }
